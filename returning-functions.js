6b5ff1fff29385171006ab5fa654d8fe6223f7e0
Dana Said <dana56@gmail.com>
2018-12-10 21:13:16 -0800

    Added the file returining-function.js

---
+++ returning-functions.js
@@ -0,0 +1,14 @@
+function one(){
+  return 'one';
+}
+
+//let value = one();
+let value = one;
+console.log(value);
+console.log(one());
+console.log(typeof value);
+
+//string
+//number
+//boolean
+//function
