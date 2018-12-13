ee533060b1726a671e665028035c000f09499270
Dana Said <dana56@gmail.com>
2018-12-12 21:12:06 -0800

    objects.js was modified

--- objects.js
+++ objects.js
@@ -28,6 +28,7 @@ console.log(a.myProperty.b);
 
 */
 
+/*
 var c = {
   myProperty: [
     { d: 'this'},
@@ -38,4 +39,22 @@ var c = {
 };
 console.log(c);
 console.log(c.myProperty[0]);
-console.log(c.myProperty[(d)]);
+*/
+
+let carLot = [
+  {year: 2017, make: Toyota, model: 'Prius'},
+  {year: 2015, make: BMW, model: '528i'},
+  {year: 1982, make: Buik, model: 'Skylark'},
+];
+
+
+let contacts = {
+  customers: [
+    {firstName: 'Bob', lastName: 'Tabor', phoneNumbers;['(123) 345-2345', '(925) 234-2342']},
+    {firstName: 'Richard', lastName: 'Smith', phoneNumbers;['(103) 355-2355', '(905) 244-2442']}
+  ],
+  employees: [
+    {firstName: 'Steve', lastName: 'Miller', phoneNumbers;['(122) 345-2445', '(325) 236-2242']},
+    {firstName: 'Conrad', lastName: 'Adenauer', phoneNumbers;['(113) 385-2555']},
+  ]
+};
